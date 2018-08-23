c38bf00c7aa2810c740467eb2437737fd9e3fe67
EstherWangari <wangarie0@gmail.com>
2018-08-23 21:29:02 +0300

    started on the main page after loging in

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ app.use(express.static('public'));
 app.get("/",function(req,res){
   res.render("login");
 });
-
+app.post("/login",function(req,res){
+  res.render("main");
+});
 
 
 app.listen(3000,function(){
